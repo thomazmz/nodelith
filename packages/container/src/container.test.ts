7e6d8e067f64c405bc1f8d5bfe792e675cca1ad3
Isadora Zandonotto <isadoraimz@gmail.com>
2025-01-06 18:58:31 -0300

    test(container): Spread injection mode

--- packages/container/src/container.test.ts
+++ packages/container/src/container.test.ts
@@ -13,12 +13,16 @@ import {
 describe('Container', () => {
 
   let targetFactoryOneWasCalled = false
+  let targetFactoryOneWithSpreadWasCalled = false
   let targetFactoryTwoWasCalled = false
+  let targetFactoryTwoWithSpreadWasCalled = false
   let targetFactoryDoubleWasCalled = false
 
   beforeEach(() => {
     targetFactoryOneWasCalled = false
+    targetFactoryOneWithSpreadWasCalled = false
     targetFactoryTwoWasCalled = false
+    targetFactoryTwoWithSpreadWasCalled = false
     targetFactoryDoubleWasCalled = false
   })
 
@@ -36,7 +40,21 @@ describe('Container', () => {
     return { call, callDependency }
   }
 
-  const targetFactoryDouble: Factory = () => {
+  const targetFactoryOneWithSpread: Injection.Target = (targetTwo) => {
+    const call = () => 'targetOne::call'
+    const callDependency = () => targetTwo.call()
+    targetFactoryOneWithSpreadWasCalled = true
+    return { call, callDependency }
+  }
+
+  const targetFactoryTwoWithSpread: Injection.Target = (targetOne) => {
+    const call = () => 'targetTwo::call'
+    const callDependency = () => targetOne.call()
+    targetFactoryTwoWithSpreadWasCalled = true
+    return { call, callDependency }
+  }
+
+  const targetFactoryDouble: Injection.Target = () => {
     const call = () => 'targetDouble::call'
     targetFactoryDoubleWasCalled = true
     return { call }
@@ -216,6 +234,158 @@ describe('Container', () => {
         expect(defaultResolverWasCalled).toBe(true)
         expect(targetFactoryOneWasCalled).toBe(true)
         expect(targetFactoryTwoWasCalled).toBe(true)
+      })
+    })
+
+    describe('Spread injection mode', () => {
+      it('Should resolve acyclic dependency graph', () => {
+        const container = new Injection.Container()
+    
+        const registrationOne = new Injection.FactoryRegistration(targetFactoryOneWithSpread, {
+          token: 'targetOne',
+          bundle: container.bundle,
+          resolver: defaultResolver,
+          mode: 'spread'
+        })
+    
+        const registrationTwo = new Injection.FactoryRegistration(targetFactoryDouble, {
+          token: 'targetTwo',
+          bundle: container.bundle,
+          resolver: defaultResolver,
+          mode: 'spread'
+        })
+    
+        container.push(
+          registrationOne,
+          registrationTwo,
+        )
+  
+        const targetOneCallResult = container.bundle.targetOne.call()
+        const targetTwoCallResult = container.bundle.targetTwo.call()
+    
+        expect(targetOneCallResult).toBe('targetOne::call')
+        expect(targetTwoCallResult).toBe('targetDouble::call')
+    
+        const targetOneCallDependencyResult = container.bundle.targetOne.callDependency()
+    
+        expect(targetOneCallDependencyResult).toBe('targetDouble::call')
+      })
+    
+      it('Should resolve cyclic dependency graph when dependency properties are not accessed during instance initialization', () => {
+        const container = new Injection.Container()
+    
+        const registrationOne = new Injection.FactoryRegistration(targetFactoryOneWithSpread, {
+          token: 'targetOne',
+          bundle: container.bundle,
+          resolver: defaultResolver,
+          mode: 'spread'
+        })
+    
+        const registrationTwo = new Injection.FactoryRegistration(targetFactoryTwoWithSpread, {
+          token: 'targetTwo',
+          bundle: container.bundle,
+          resolver: defaultResolver,
+          mode: 'spread'
+        })
+    
+        container.push(
+          registrationOne,
+          registrationTwo,
+        )
+    
+        const targetOneCallResult = container.bundle.targetOne.call()
+        const targetTwoCallResult = container.bundle.targetTwo.call()
+    
+        expect(targetOneCallResult).toBe('targetOne::call')
+        expect(targetTwoCallResult).toBe('targetTwo::call')
+    
+        const targetOneCallDependencyResult = container.bundle.targetOne.callDependency()
+        const targetTwoCallDependencyResult = container.bundle.targetTwo.callDependency()
+        
+        expect(targetOneCallDependencyResult).toBe('targetTwo::call')
+        expect(targetTwoCallDependencyResult).toBe('targetOne::call')
+      })
+    
+      it('Should resolve cyclic dependency graph when dependency properties are accessed in single direction', () => {
+        const container = new Injection.Container()
+    
+        const registrationOne = new Injection.FactoryRegistration(targetFactoryOneWithSpread, {
+          token: 'targetOne',
+          bundle: container.bundle,
+          resolver: defaultResolver,
+          mode: 'spread'
+        })
+    
+        const registrationTwo = new Injection.FactoryRegistration((targetOne) => {
+          const targetOneCallResultDuringResolution = targetOne.call()
+          expect(targetOneCallResultDuringResolution).toBe('targetOne::call')
+          return targetFactoryTwoWithSpread(targetOne)
+        }, {
+          token: 'targetTwo',
+          bundle: container.bundle,
+          resolver: defaultResolver,
+          mode: 'spread'
+        })
+    
+        container.push(
+          registrationOne,
+          registrationTwo,
+        )
+    
+        const targetOneCallResult = container.bundle.targetOne.call()
+        const targetTwoCallResult = container.bundle.targetTwo.call()
+    
+        expect(targetOneCallResult).toBe('targetOne::call')
+        expect(targetTwoCallResult).toBe('targetTwo::call')
+    
+        const targetOneCallDependencyResult = container.bundle.targetOne.callDependency()
+        const targetTwoCallDependencyResult = container.bundle.targetTwo.callDependency()
+        
+        expect(targetOneCallDependencyResult).toBe('targetTwo::call')
+        expect(targetTwoCallDependencyResult).toBe('targetOne::call')
+      })
+    
+      it('Should lazily resolve dependency targets only when dependency properties are accessed', () => {
+        const container = new Injection.Container()
+  
+        const registrationOne = new Injection.FactoryRegistration(targetFactoryOneWithSpread, {
+          token: 'targetOne',
+          bundle: container.bundle,
+          resolver: defaultResolver,
+          mode: 'spread'
+        })
+        
+        const registrationTwo = new Injection.FactoryRegistration(targetFactoryTwoWithSpread, {
+          token: 'targetTwo',
+          bundle: container.bundle,
+          resolver: defaultResolver,
+          mode: 'spread'
+        })
+    
+        container.push(
+          registrationOne,
+          registrationTwo,
+        )
+    
+        expect(defaultResolverWasCalled).toBe(false)
+        expect(targetFactoryOneWithSpreadWasCalled).toBe(false)
+        expect(targetFactoryTwoWithSpreadWasCalled).toBe(false)
+    
+        container.bundle.targetOne
+    
+        expect(defaultResolverWasCalled).toBe(false)
+        expect(targetFactoryOneWithSpreadWasCalled).toBe(false)
+        expect(targetFactoryTwoWithSpreadWasCalled).toBe(false)
+    
+        container.bundle.targetOne.call
+        expect(defaultResolverWasCalled).toBe(true)
+        expect(targetFactoryOneWithSpreadWasCalled).toBe(true)
+        expect(targetFactoryTwoWithSpreadWasCalled).toBe(false)
+        
+        container.bundle.targetTwo.call
+        expect(defaultResolverWasCalled).toBe(true)
+        expect(targetFactoryOneWithSpreadWasCalled).toBe(true)
+        expect(targetFactoryTwoWithSpreadWasCalled).toBe(true)
       })
     })
   })
