8692284937f1f69eac382fd319bc5f48afb2145f
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-01-11 14:02:20 -0800

    chore(@nodelith/injection): Transform Module interface into implementation scaffolding

--- packages/injection/src/module.ts
+++ packages/injection/src/module.ts
@@ -6,59 +6,93 @@ import { Token } from './token'
 import { Options } from './options' 
 import { Registration } from './registration'
 
-export interface Module {
+export class Module {
 
-  registerStatic<R>(token: Token, resolution: R): Registration<R>
-
-  registerConstructor<R extends InstanceType<Types.Constructor>>(token: Token, constructor: Types.Constructor<R>, options?: Options): Registration<R>
-
-  registerFactory<R extends ReturnType<Types.Factory>>(token: Token, factory: Types.Factory<R>, options?: Options): Registration<R>
-
-  registerResolver<R extends ReturnType<Types.Resolver>>(token: Token, resolver: Types.Resolver<R>, options?: Options): Registration<R>
-
-  register<R>(token: Token, options: {
+  public register<R>(token: Token, options: {
     static: R
   }): Registration<R>
 
-  register<R extends InstanceType<Types.Constructor>>(token: Token, options: Options & {
+  public register<R extends InstanceType<Types.Constructor>>(token: Token, options: Options & {
     constructor: Types.Constructor<R>,
   }): Registration<R>
 
-  register<R extends ReturnType<Types.Factory>>(token: Token, options: Options & {
+  public register<R extends ReturnType<Types.Factory>>(token: Token, options: Options & {
     factory: Types.Factory<R>
   }): Registration<R>
 
-  register<R extends ReturnType<Types.Resolver>>(token: Token, options: Options & {
+  public register<R extends ReturnType<Types.Resolver>>(token: Token, options: Options & {
     resolver: Types.Resolver<R>,
   }): Registration<R>
 
-  registerInitializer<I extends Core.Initializer>(token: Token, options: {
+  public register<R>(token: Token, options:
+    | { static: R }
+    | { factory: Types.Factory } & Options
+    | { resolver: Types.Resolver } & Options
+    | { constructor: Types.Constructor } & Options
+  ):  Registration<R> {
+    throw new Error('Method not implemented.')
+  }
+
+  public registerStatic<R>(token: Token, resolution: R): Registration<R> {
+    throw new Error('Method not implemented')
+  }
+
+  public registerConstructor<R extends InstanceType<Types.Constructor>>(token: Token, constructor: Types.Constructor<R>, options?: Options): Registration<R> {
+    throw new Error('Method not implemented')
+  }
+
+  public registerFactory<R extends ReturnType<Types.Factory>>(token: Token, factory: Types.Factory<R>, options?: Options): Registration<R> {
+    throw new Error('Method not implemented')
+  }
+
+  public registerResolver<R extends ReturnType<Types.Resolver>>(token: Token, resolver: Types.Resolver<R>, options?: Options): Registration<R> {
+    throw new Error('Method not implemented')
+  }
+
+  public registerInitializer<I extends Core.Initializer>(token: Token, options: {
     factory: Types.Factory<I>
     mode?: Mode,
   }): Registration<I>
 
-  registerInitializer<I extends Core.Initializer>(token: Token, options: {
+  public registerInitializer<I extends Core.Initializer>(token: Token, options: {
     constructor: Types.Constructor<I>
     mode?: Mode,
   }): Registration<I>
 
-  registerInitializerConstructor<I extends Core.Initializer>(token: Token, constructor: Types.Constructor<I>, options?: {
-    mode?: Mode,
-  }): Registration<I>
+  public registerInitializer<I extends Core.Initializer>(token: Token, options:
+    | { factory: Types.Factory<I>, mode?: Mode }
+    | { constructor: Types.Constructor<I>, mode?: Mode }
+  ): Registration<I> {
+    throw new Error('Method not implemented.')
+  }
 
-  registerInitializerFactory<I extends Core.Initializer>(token: Token, factory: Types.Factory<I>, options?: {
+  public registerInitializerConstructor<I extends Core.Initializer>(token: Token, constructor: Types.Constructor<I>, options?: {
     mode?: Mode,
-  }): Registration<I>
+  }): Registration<I> {
+    throw new Error('Method not implemented.')
+  }
 
-  resolve<R extends ReturnType<Types.Resolver>>(resolver: Types.Resolver<R>, options?: {
+  public registerInitializerFactory<I extends Core.Initializer>(token: Token, factory: Types.Factory<I>, options?: {
     mode?: Mode,
-  }): Registration<R>
+  }): Registration<I> {
+    throw new Error('Method not implemented.')
+  }
 
-  resolveConstructor<R extends InstanceType<Types.Constructor>>(constructor: Types.Constructor<R>, options?: {
+  public resolve<R extends ReturnType<Types.Resolver>>(resolver: Types.Resolver<R>, options?: {
     mode?: Mode,
-  }): Registration<R>
+  }): Registration<R> {
+    throw new Error('Method not implemented.')
+  }
 
-  resolveFactory<R extends ReturnType<Types.Factory>>(factory: Types.Factory<R>, options?: {
+  public resolveConstructor<R extends InstanceType<Types.Constructor>>(constructor: Types.Constructor<R>, options?: {
     mode?: Mode,
-  }): Registration<R>
+  }): Registration<R> {
+    throw new Error('Method not implemented.')
+  }
+
+  public resolveFactory<R extends ReturnType<Types.Factory>>(factory: Types.Factory<R>, options?: {
+    mode?: Mode,
+  }): Registration<R> {
+    throw new Error('Method not implemented.')
+  }
 }
