d993b4e930d20d8ffe01d2b85dc9b84ef4166622
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-03-07 00:11:37 -0800

    implement ConstructorRegistration tests

--- packages/injection/src/registration/implementations/constructor-registration.ts
+++ packages/injection/src/registration/implementations/constructor-registration.ts
@@ -14,22 +14,67 @@ export type ConstructorRegistrationOptions = {
 }
 
 export class ConstructorRegistration<R extends object> implements Registration<R> {
-  public static create<R extends object>(target: Types.Constructor<R>, options: ConstructorRegistrationOptions): ConstructorRegistration<R> {
-    throw new Error('Static method ConstructorRegistration:create is mot Implemented')
+  public static create<R extends object>(target: Types.Constructor<R>, options?: ConstructorRegistrationOptions): ConstructorRegistration<R> {
+    return new ConstructorRegistration(target, options)
   }
 
-  public get token(): Token {
-    throw new Error('ConstructorRegistration class is mot Implemented')
-  }
+  private readonly singleton: { resolution?: R } = { }
 
-  public get access(): Access {
-    throw new Error('ConstructorRegistration class is mot Implemented')
+  private readonly target: Types.Constructor<R>
+
+  public readonly access: Access
+
+  private readonly bundle: Bundle
+
+  public readonly lifetime: Lifetime
+
+  public token: Token
+
+  public constructor(target: Types.Constructor<R>, options?: ConstructorRegistrationOptions) {
+    this.token = options?.token ?? Symbol()
+    this.bundle = options?.bundle ?? {}
+    this.access = options?.access ?? 'public'
+    this.lifetime = options?.lifetime ?? 'singleton'
+    this.target = target
   }
 
   public clone(bundle?: Bundle): Registration<R> {
-    throw new Error('ConstructorRegistration.clone method is mot Implemented')
+    return new ConstructorRegistration<R>(this.target, {
+      token: this.token,
+      access: this.access,
+      lifetime: this.lifetime,
+      bundle,
+    })
   }
+
   public resolve(bundle?: Bundle): R {
-    throw new Error('ConstructorRegistration.resolve method is mot Implemented')
+    if('resolution' in this.singleton) {
+      return this.singleton.resolution
+    }
+
+    const resolutionBundle = this.createResolutionBundle(bundle)
+
+    if(this.lifetime === 'singleton') {
+      return this.singleton.resolution = new this.target(resolutionBundle)
+    }
+
+    return new this.target(resolutionBundle)
+  };
+
+  private createResolutionBundle(bundle: Bundle = {}): Bundle {
+    return new Proxy(bundle, {
+      set: (_target: Bundle, token: Token) => {
+        throw new Error(`Could not set bundle key "${token.toString()}". Targets are not allowed to assign bundle values.`)
+      },
+      get: (target, token) => {
+        if(token  === this.token) {
+          throw new Error(`Could not access bundle key "${token.toString()}". Target should not access its own token.`)
+        }
+        if(token in this.bundle) {
+          return this.bundle[token]
+        }
+        return target[token]
+      } 
+    })
   }
 }
