36731e2de2ee15ea247f46f1aafc429cd7db1e69
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-10-13 01:51:00 -0700

    Implement InjectionFacade

---
+++ packages/injection/src/injection-facade.ts
@@ -0,0 +1,32 @@
+import { ObjectUtils } from '@nodelith/utils'
+import { ConstructorUtils } from '@nodelith/utils'
+import { InjectionModule, InjectionRegistration } from '@nodelith/injection'
+import { InjectionBundle } from '@nodelith/injection'
+
+export type InjectionFacade<T extends object> = Pick<T, {
+  [K in keyof T]-?: T[K] extends (this: any, ...a: any[]) => any ? K : never
+}[keyof T]>;
+
+export function createInjectionFacade<T extends ConstructorUtils>(module: InjectionModule, constructor: T, bundle?: InjectionBundle): InjectionFacade<InstanceType<T>> {
+  return Object.freeze(ObjectUtils.extractMembers(constructor.prototype).reduce((gateway, member) => {
+    if(ObjectUtils.isConstructorMember(member) || !ObjectUtils.isFunctionMember(member)) {
+      return gateway
+    }
+
+    const operation = (...args: any[]) => {
+      const instance = InjectionModule.resolve(module, { class: constructor, bundle })
+      return instance[member.key].apply(instance, args)
+    }
+
+    return Object.defineProperty(gateway, member.key, {
+      value: operation,
+      configurable: false,
+      enumerable: true,
+      writable: false,
+    })
+  }, {} as InjectionFacade<InstanceType<T>>))
+}
+
+export const InjectionFacade = Object.freeze({
+  create: createInjectionFacade,
+});
