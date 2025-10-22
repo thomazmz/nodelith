4f3cf7e25dc830cb770d23da913e88bf5fc24414
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2025-10-21 17:56:08 -0700

    Change CoreValidator methods to accept Error constructor

--- packages/core/src/core-validator.ts
+++ packages/core/src/core-validator.ts
@@ -1,20 +1,22 @@
-export abstract class CoreValidator<T = any> {
-  abstract validate(value: unknown): Readonly<[undefined, Error] | [T, undefined]>
+import { ConstructorUtils } from '@nodelith/utils'
+
+export abstract class CoreValidator<T> {
+  public abstract validate(value: unknown): Readonly<[undefined, string] | [T, undefined]>
 
   public match(value: unknown): value is T {
     const [_result, error] = this.validate(value)
-    return error ? false : true
+    return error === undefined
   }
 
-  public assert(value: unknown): true | never {
+  public assert(value: unknown, throwable: ConstructorUtils<Error, [string]> = Error): true | never {
     const [_result, error] = this.validate(value)
-    if(error) throw error
+    if (error !== undefined) throw new throwable(error)
     return true
   }
 
-  public parse(value: unknown): T {
-    const [ result, error ] = this.validate(value)
-    if(error) throw error
+  public parse(value: unknown, throwable: ConstructorUtils<Error, [string]> = Error): T {
+    const [result, error] = this.validate(value)
+    if (error !== undefined) throw new throwable(error)
     return result
   }
 }
