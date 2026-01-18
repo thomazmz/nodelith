47290eff69c8678acc0ce9853e1a4024c3e9887d
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2026-01-18 00:50:49 -0800

    Fix environment variable loading

--- example/src/application/config.ts
+++ example/src/application/config.ts
@@ -3,7 +3,7 @@ import { ConfigProfile } from "@nodelith/config";
 
 export class ApplicationConfig extends ExpressConfig {
   public override readonly profile = Object.freeze({
-    name: ConfigProfile.string('APPLICATION_SERVER_NAME', 'Pet Registry'),
+    name: ConfigProfile.string('APPLICATION_SERVER_NAME', 'Pet Service'),
     port: ConfigProfile.number('APPLICATION_SERVER_PORT', 4004),
   })
 }
