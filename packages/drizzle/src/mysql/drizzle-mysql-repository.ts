2d63d2f9b798b722fc62fd3ec814c09677781e8f
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2026-02-03 03:31:53 -0800

    chore(@nodelith/drizzle): Implement empty placeholder methods

--- packages/drizzle/src/mysql/drizzle-mysql-repository.ts
+++ packages/drizzle/src/mysql/drizzle-mysql-repository.ts
@@ -16,52 +16,79 @@ export abstract class DrizzleMysqlRepository<E extends CoreEntity, T extends Dri
     this.table = mysqlTable
   }
 
-  protected static create = <E extends CoreEntity>(database: MySql2Database, table: DrizzleMysqlTable) => async (entries: CoreEntity.Entries): Promise<DrizzleMysqlTable['$inferSelect']> => {
+  protected static createOne = <E extends CoreEntity>(database: MySql2Database, table: DrizzleMysqlTable) => async (entries: CoreEntity.Entries): Promise<DrizzleMysqlTable['$inferSelect']> => {
     const [ response ] = await database.insert(table).values(entries).execute()
-    return DrizzleMysqlRepository.getById(database, table)(response.insertId)
+    return DrizzleMysqlRepository.getOneById(database, table)(response.insertId)
   }
 
-  protected static updateById = (database: MySql2Database, table: DrizzleMysqlTable) => async (id: string | number, properties: Partial<CoreEntity.Entries>): Promise<DrizzleMysqlTable['$inferSelect']> => {
+  protected static updateOneById = (database: MySql2Database, table: DrizzleMysqlTable) => async (id: string | number, properties: Partial<CoreEntity.Entries>): Promise<DrizzleMysqlTable['$inferSelect']> => {
     await database.update(table).set(properties).where(eq(getTableColumns(table).id, Number(id))).execute()
-    return DrizzleMysqlRepository.getById(database, table)(id)
+    return DrizzleMysqlRepository.getOneById(database, table)(id)
   }
 
-  protected static deleteById = (database: MySql2Database, table: DrizzleMysqlTable) => async (id: string | number): Promise<void> => {
+  protected static deleteOneById = (database: MySql2Database, table: DrizzleMysqlTable) => async (id: string | number): Promise<void> => {
     await database.delete(table).where(eq(getTableColumns(table).id, Number(id))).execute()
   }
 
-  protected static getById = (database: MySql2Database, table: DrizzleMysqlTable) => async (id: string | number): Promise<DrizzleMysqlTable['$inferSelect']> => {
+  protected static getOneById = (database: MySql2Database, table: DrizzleMysqlTable) => async (id: string | number): Promise<DrizzleMysqlTable['$inferSelect']> => {
     const [ row ] = await database.select().from(table).where(eq(getTableColumns(table).id, Number(id))).limit(1).execute()
     return row
   }
 
-  protected static getAll  = (database: MySql2Database, table: DrizzleMysqlTable) => async (): Promise<DrizzleMysqlTable['$inferSelect'][]> => {
+  protected static findAll = (database: MySql2Database, table: DrizzleMysqlTable) => async (): Promise<DrizzleMysqlTable['$inferSelect'][]> => {
     return database.select().from(table).execute()
+  }
+
+  protected static findOneById = (database: MySql2Database, table: DrizzleMysqlTable) => async (id: string | number): Promise<DrizzleMysqlTable['$inferSelect'] | undefined> => {
+    throw new Error('Method not implemented')
+  }
+
+  protected static findByQuery = (database: MySql2Database, table: DrizzleMysqlTable) => async (query: Record<string, unknown>): Promise<DrizzleMysqlTable['$inferSelect'][]> => {
+    throw new Error('Method not implemented')
+  }
+
+  protected static findOneByQuery = (database: MySql2Database, table: DrizzleMysqlTable) => async (query: Record<string, unknown>): Promise<DrizzleMysqlTable['$inferSelect'] | undefined> => {
+    throw new Error('Method not implemented')
   }
 
   protected abstract mapEntity(row: DrizzleMysqlTable['$inferSelect']): E
 
-  public async create(entries: CoreEntity.Entries<E>): Promise<E> {
-    const drizzleResult = await DrizzleMysqlRepository.create<E>(this.database, this.table)(entries)
+  public async createOne(entries: CoreEntity.Entries<E>): Promise<E> {
+    const drizzleResult = await DrizzleMysqlRepository.createOne<E>(this.database, this.table)(entries)
     return this.mapEntity(drizzleResult)
   }
 
-  public async updateById(id: E['id'], entries: Partial<CoreEntity.Entries<E>>): Promise<E> {
-    const drizzleResult = await DrizzleMysqlRepository.updateById(this.database, this.table)(id, entries)
+  public async updateOneById(id: E['id'], entries: Partial<CoreEntity.Entries<E>>): Promise<E> {
+    const drizzleResult = await DrizzleMysqlRepository.updateOneById(this.database, this.table)(id, entries)
     return this.mapEntity(drizzleResult)
   }
 
-  public async deleteById(id: E['id']): Promise<void> {
-    const drizzleResult = await DrizzleMysqlRepository.deleteById(this.database, this.table)(id)
+  public async deleteOneById(id: E['id']): Promise<void> {
+    const drizzleResult = await DrizzleMysqlRepository.deleteOneById(this.database, this.table)(id)
   }
 
-  public async getById(id: E['id']): Promise<E> {
-    const drizzleResult = await DrizzleMysqlRepository.getById(this.database, this.table)(id)
+  public async getOneById(id: E['id']): Promise<E> {
+    const drizzleResult = await DrizzleMysqlRepository.getOneById(this.database, this.table)(id)
     return this.mapEntity(drizzleResult)
   }
 
-  public async getAll(): Promise<E[]> {
-    const drizzleResult = await  DrizzleMysqlRepository.getAll(this.database, this.table)()
+  public async findOneById(id: E['id']): Promise<E | undefined> {
+    const drizzleResult = await DrizzleMysqlRepository.findOneById(this.database, this.table)(id)
+    return drizzleResult ? this.mapEntity(drizzleResult) : undefined
+  }
+
+  public async findByQuery(query: Partial<E>): Promise<E[]> {
+    const drizzleResult = await DrizzleMysqlRepository.findByQuery(this.database, this.table)(query as any)
+    return drizzleResult.map((singleResult) => this.mapEntity(singleResult))
+  }
+
+  public async findOneByQuery(query: Partial<E>): Promise<E | undefined> {
+    const drizzleResult = await DrizzleMysqlRepository.findOneByQuery(this.database, this.table)(query as any)
+    return drizzleResult ? this.mapEntity(drizzleResult) : undefined
+  }
+
+  public async findAll(): Promise<E[]> {
+    const drizzleResult = await  DrizzleMysqlRepository.findAll(this.database, this.table)()
     return drizzleResult.map(singleResult => this.mapEntity(singleResult))
   }
 }
--- packages/drizzle/src/postgres/drizzle-postgres-repository.ts
+++ packages/drizzle/src/postgres/drizzle-postgres-repository.ts
@@ -11,58 +11,84 @@ export abstract class DrizzlePostgresRepository<E extends CoreEntity, T extends 
   protected readonly database: NodePgDatabase
   protected readonly table: T
 
-  public constructor(pgPool: Pool, pgTable: T) {
-    this.database = drizzle(pgPool)
-    this.table = pgTable
+  public constructor(postgresPool: Pool, postgresTable: T) {
+    this.database = drizzle(postgresPool)
+    this.table = postgresTable
   }
 
-  protected static create = <E extends CoreEntity>(database: NodePgDatabase, table: DrizzlePostgresTable) => async (entries: CoreEntity.Entries): Promise<DrizzlePostgresTable['$inferSelect']> => {
+  protected static createOne = <E extends CoreEntity>(database: NodePgDatabase, table: DrizzlePostgresTable) => async (entries: CoreEntity.Entries): Promise<DrizzlePostgresTable['$inferSelect']> => {
     const [ row ] = await database.insert(table).values(entries).returning().execute()
     return row
   }
 
-  protected static updateById = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (id: string | number, properties: Partial<CoreEntity.Entries>): Promise<DrizzlePostgresTable['$inferSelect']> => {
+  protected static updateOneById = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (id: string | number, properties: Partial<CoreEntity.Entries>): Promise<DrizzlePostgresTable['$inferSelect']> => {
     const [ row ] = await database.update(table).set(properties).where(eq(getTableColumns(table).id, id)).returning().execute()
     return row
   }
 
-  protected static deleteById = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (id: string | number): Promise<void> => {
+  protected static deleteOneById = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (id: string | number): Promise<void> => {
     await database.delete(table).where(eq(getTableColumns(table).id, id)).execute()
   }
 
-  protected static getById = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (id: string | number): Promise<DrizzlePostgresTable['$inferSelect']> => {
+  protected static getOneById = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (id: string | number): Promise<DrizzlePostgresTable['$inferSelect']> => {
     const [ row ] = await database.select().from(table).where(eq(getTableColumns(table).id, id)).limit(1).execute()
     return row
   }
 
-  protected static getAll = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (): Promise<DrizzlePostgresTable['$inferSelect'][]> => {
+  protected static findAll = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (): Promise<DrizzlePostgresTable['$inferSelect'][]> => {
     return database.select().from(table).execute()
+  }
+
+  protected static findOneById = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (id: string | number): Promise<DrizzlePostgresTable['$inferSelect'] | undefined> => {
+    throw new Error('Method not implemented')
+  }
+
+  protected static findByQuery = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (query: Record<string, unknown>): Promise<DrizzlePostgresTable['$inferSelect'][]> => {
+    throw new Error('Method not implemented')
+  }
+
+  protected static findOneByQuery = (database: NodePgDatabase, table: DrizzlePostgresTable) => async (query: Record<string, unknown>): Promise<DrizzlePostgresTable['$inferSelect'] | undefined> => {
+    throw new Error('Method not implemented')
   }
 
   protected abstract mapEntity(row: DrizzlePostgresTable['$inferSelect']): E
 
-  public async create(entries: CoreEntity.Entries<E>): Promise<E> {
-    const drizzleResult = await DrizzlePostgresRepository.create<E>(this.database, this.table)(entries)
+  public async createOne(entries: CoreEntity.Entries<E>): Promise<E> {
+    const drizzleResult = await DrizzlePostgresRepository.createOne<E>(this.database, this.table)(entries)
     return this.mapEntity(drizzleResult)
   }
 
-  public async updateById(id: E['id'], entries: Partial<CoreEntity.Entries<E>>): Promise<E> {
-    const drizzleResult = await DrizzlePostgresRepository.updateById(this.database, this.table)(id, entries)
+  public async updateOneById(id: E['id'], entries: Partial<CoreEntity.Entries<E>>): Promise<E> {
+    const drizzleResult = await DrizzlePostgresRepository.updateOneById(this.database, this.table)(id, entries)
     return this.mapEntity(drizzleResult)
   }
 
-  public async deleteById(id: E['id']): Promise<void> {
-    await DrizzlePostgresRepository.deleteById(this.database, this.table)(id)
+  public async deleteOneById(id: E['id']): Promise<void> {
+    await DrizzlePostgresRepository.deleteOneById(this.database, this.table)(id)
   }
 
-  public async getById(id: E['id']): Promise<E> {
-    const drizzleResult = await DrizzlePostgresRepository.getById(this.database, this.table)(id)
+  public async getOneById(id: E['id']): Promise<E> {
+    const drizzleResult = await DrizzlePostgresRepository.getOneById(this.database, this.table)(id)
     return this.mapEntity(drizzleResult)
   }
 
-  public async getAll(): Promise<E[]> {
-    const drizzleResult = await DrizzlePostgresRepository.getAll(this.database, this.table)()
+  public async findOneById(id: E['id']): Promise<E | undefined> {
+    const drizzleResult = await DrizzlePostgresRepository.findOneById(this.database, this.table)(id)
+    return drizzleResult ? this.mapEntity(drizzleResult) : undefined
+  }
+
+  public async findByQuery(query: Partial<E>): Promise<E[]> {
+    const drizzleResult = await DrizzlePostgresRepository.findByQuery(this.database, this.table)(query as any)
+    return drizzleResult.map((singleResult) => this.mapEntity(singleResult))
+  }
+
+  public async findOneByQuery(query: Partial<E>): Promise<E | undefined> {
+    const drizzleResult = await DrizzlePostgresRepository.findOneByQuery(this.database, this.table)(query as any)
+    return drizzleResult ? this.mapEntity(drizzleResult) : undefined
+  }
+
+  public async findAll(): Promise<E[]> {
+    const drizzleResult = await DrizzlePostgresRepository.findAll(this.database, this.table)()
     return drizzleResult.map((singleResult) => this.mapEntity(singleResult))
   }
 }
-
